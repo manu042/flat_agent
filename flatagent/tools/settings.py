5206a54e2b48601d2866d96e5e43cf2770ed6143
manu042 <manu42x@gmail.com>
2020-07-30 11:48:09 +0200

    Replace yaml config with environment variables

--- flatagent/tools/settings.py
+++ flatagent/tools/settings.py
@@ -1,32 +1,25 @@
 import os
-import yaml
 from tools.decorators import singleton
 
 
 @singleton
 class Settings:
-
     BASE_PATH = os.path.abspath(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
     LOG_FILE_PATH = os.path.join(BASE_PATH, "logs", "flat_agent.log")
     JSON_FILE_PATH = os.path.join(BASE_PATH, "data", "real_estate.json")
 
     def __init__(self):
-        # load config file
-        config_path = os.path.join(self.BASE_PATH, "flatagent", "tools", "config.yaml")
-        with open(config_path) as fp:
-            config = yaml.load(fp, Loader=yaml.FullLoader)
-
         # Load Mail settings
-        self.EMAIL_HOST = config["MAIL_SETTINGS"]["EMAIL_HOST"]
-        self.EMAIL_HOST_PORT = config["MAIL_SETTINGS"]["EMAIL_HOST_PORT"]
-        self.EMAIL_HOST_USER = config["MAIL_SETTINGS"]["EMAIL_HOST_USER"]
-        self.EMAIL_HOST_PASSWORD = config["MAIL_SETTINGS"]["EMAIL_HOST_PASSWORD"]
-        self.DEFAULT_FROM_EMAIL = config["MAIL_SETTINGS"]["DEFAULT_FROM_EMAIL"]
-        self.DEFAULT_TO_EMAIL = config["MAIL_SETTINGS"]["DEFAULT_TO_EMAIL"]
+        self.EMAIL_HOST = os.environ["EMAIL_HOST"]
+        self.EMAIL_HOST_PORT = os.environ["EMAIL_HOST_PORT"]
+        self.EMAIL_HOST_USER = os.environ["EMAIL_HOST_USER"]
+        self.EMAIL_HOST_PASSWORD = os.environ["EMAIL_HOST_PASSWORD"]
+        self.DEFAULT_FROM_EMAIL = os.environ["DEFAULT_FROM_EMAIL"]
+        self.DEFAULT_TO_EMAIL = os.environ["DEFAULT_TO_EMAIL"]
 
         # Load Immobilienscout24 settings
-        self.SEARCH_URL = config["IMMO_SCOUT_24"]["SEARCH_URL"]
-        self.ATTACHMENT = config["IMMO_SCOUT_24"]["ATTACHMENT"]
+        self.SEARCH_URL = os.environ["SEARCH_URL"]
+        self.ATTACHMENT = os.environ.get("ATTACHMENT", False)
 
 
 settings = Settings()
